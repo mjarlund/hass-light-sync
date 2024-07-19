ccfb38ea0e33ab51e5ff8056f07d632855169a35
Mikkel Jarlund <mikkel.jarlund@gmail.com>
2024-07-19 09:36:55 +0200

    refactor: minor improvements

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,12 @@ use crate::capture::{capture_frame, calculate_average_color, smooth_colors};
 use crate::api::{send_rgb};
 use captrs::Capturer;
 use std::sync::Arc;
-use std::thread;
 use std::time::Duration;
-use tokio::spawn;
-use log::{info, debug, error};
+use log::{info, error};
 use env_logger::Env;
 use colored::*;
 use std::collections::{HashMap, HashSet};
+use rayon::prelude::*;
 
 mod settings;
 mod capture;
@@ -17,7 +16,7 @@ mod api;
 mod models;
 
 fn rgb_to_ansi_color(r: u32, g: u32, b: u32) -> ColoredString {
-    let color_string = format!("⬤");
+    let color_string = "⬤".to_string();
     color_string.truecolor(r as u8, g as u8, b as u8)
 }
 
@@ -25,53 +24,57 @@ fn rgb_to_ansi_color(r: u32, g: u32, b: u32) -> ColoredString {
 async fn main() {
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
 
-    info!("Loading settings from 'settings.json'.");
     let settings = load_settings("settings.json");
     let client = Arc::new(reqwest::Client::new());
-
-    info!("Initializing capture with monitor ID: {}", settings.monitor_id);
     let mut capturer = Capturer::new(settings.monitor_id as usize).expect("Failed to get Capture Object");
     let mut prev_avg_colors: HashMap<String, (u32, u32, u32)> = HashMap::new();
 
     loop {
         if let Some(frame) = capture_frame(&mut capturer) {
-            debug!("Frame captured successfully.");
-
-            // Collect unique positions
-            let unique_positions: Vec<String> = settings.lights.iter()
+            // Convert HashSet to Vec for parallel processing
+            let unique_positions: Vec<_> = settings.lights.iter()
                 .map(|light| light.position.clone())
-                .collect::<HashSet<_>>()
+                .collect::<HashSet<_>>()  // Collect positions into HashSet to deduplicate
                 .into_iter()
-                .collect();
+                .collect();  // Convert to Vec
 
-            // Calculate color once per unique position
-            let position_colors: HashMap<String, [u32; 3]> = unique_positions.iter()
+            // Parallel processing of positions to calculate colors
+            let position_colors: HashMap<String, [u32; 3]> = unique_positions.par_iter()
                 .map(|pos| (pos.clone(), calculate_average_color(&frame, pos, settings.skip_pixels)))
                 .collect();
 
-            for light in settings.lights.iter() {
+            let futures: Vec<_> = settings.lights.iter().map(|light| {
                 let new_color = position_colors.get(&light.position).unwrap();
                 let smoothed_color = smooth_colors(
                     *prev_avg_colors.entry(light.position.clone()).or_insert((0, 0, 0)),
                     *new_color,
                     settings.smoothing_factor
                 );
-
                 prev_avg_colors.insert(light.position.clone(), smoothed_color);
                 let brightness = smoothed_color.0.max(smoothed_color.1).max(smoothed_color.2);
 
-                let api_endpoint = settings.api_endpoint.clone();
-                let token = settings.token.clone();
-                spawn(send_rgb(Arc::clone(&client), api_endpoint, token, vec![smoothed_color.0, smoothed_color.1, smoothed_color.2], brightness, light.entity_name.clone()));
+                send_rgb(
+                    Arc::clone(&client),
+                    settings.api_endpoint.clone(),
+                    settings.token.clone(),
+                    vec![smoothed_color.0, smoothed_color.1, smoothed_color.2],
+                    brightness,
+                    light.entity_name.clone()
+                )
+            }).collect();
+
+            let _results = futures::future::join_all(futures).await;
+
+            for light in &settings.lights {
+                let smoothed_color = prev_avg_colors.get(&light.position).unwrap();
                 let color_circle = rgb_to_ansi_color(smoothed_color.0, smoothed_color.1, smoothed_color.2);
-                info!("Sending RGB data for {} at position {}: {}", light.entity_name, light.position, color_circle);
+                info!("Sent RGB data for {} at position {}: {}", light.entity_name, light.position, color_circle);
             }
         } else {
             error!("Failed to capture frame.");
         }
 
-        thread::sleep(Duration::from_millis(settings.grab_interval as u64));
-        debug!("Sleeping for {} milliseconds", settings.grab_interval);
+        tokio::time::sleep(Duration::from_millis(settings.grab_interval as u64)).await;
     }
 }
 
